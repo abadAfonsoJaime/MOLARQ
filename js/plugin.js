34adc010f1f8df819e4367c243d08ae496b9e4d2
Unknown <jaime.abad.afonso@gmail.com>
2018-01-10 14:34:12 +0100

    miércoles 14:30

--- js/plugin.js
+++ js/plugin.js
@@ -1,40 +1,194 @@
 $(document).ready(function() {
-  // Begin of map
+
+// Begin of map
+
+document.getElementById("Provincia").value = "default";
+document.getElementById("Cronologia").value = "default";
+
   $('#map_canvas').mapit({
-    latitude:    40.4167754, // latitud Madrid
-    longitude:   -3.7037901, // longitud Madrid
-    zoom:        8,
+    latitude:    40.4167754, /*coordenadas Madrid*/
+    longitude:   1.482148, /*coordenadas Madrid*/
+    zoom:        6,
     type:        'ROADMAP',
     scrollwheel: false,
     marker: {
-      latitude:   40.4167754,/* Marcador por defecto */
-      longitude:  -3.7037901,
-      icon:       'img/marker_red.png',
-      title:      'The Hotel',
+      latitude:   39.52001,/* Marcador por defecto */
+      longitude:  1.482148,
+      icon:       'img/black_red.png',
+      title:      '',
       open:       false,
       center:     true
     },
     address: '<h2>The Hotel</h2><p>Address 1, Area - County<br />Athens 123 45, Greece</p><p>Tel.: +30 210 123 4567<br />Fax: +30 210 123 4567</p>',
     styles: 'GRAYSCALE',
     locations: [
-      [37.999252, 23.733353, 'img/marker_blue.png', 'Cinema Aello 5+1', 'Panagi Tsaldari 140, Athina, Greece', false, '1'],
-      [37.996547, 23.732001, 'img/marker_blue.png', 'Cinema Ilion', 'Τροίας 34, Αθήνα, Greece', false, '1'],
-      [37.959408, 23.713982, 'img/marker_blue.png', 'Cinema Calipso', 'Megalou Alexandrou ke Kalipsous, Kallithea, Greece', false, '1'],
-      [37.977563, 23.714041, 'img/marker_green.png', 'Technopolis', 'Πειραιώς 100, Αθήνα, Greece', false, '2'],
-      [37.971436, 23.736695, 'img/marker_green.png', 'Zappeion', 'Greece', false, '2'],
-      [37.975030, 23.747751, 'img/marker_green.png', 'Εθνική Πινακοθήκη Μουσείο Αλεξάνδρου Σούτζου', 'Μιχαλακοπούλου 1, Αθήνα, Greece', false, '2'],
-      [37.976104, 23.7141811, 'img/marker_green.png', 'Benaki Museum', 'Koumpari 1, Athens, Greece', false, '2']
+      [39.920491, -2.804128, 'img/marker_blue.png', 'Parque arqueológico', 'Saelices, Cuenca', false, 'CUE'],
+      [41.193839, 1.566456, 'img/marker_blue.png', 'Ciudadela Ibérica', 'Calafell, Tarragona', false, 'TAR'],
+      [42.1198504, 3.1227702, 'img/marker_blue.png', 'Ampurias', 'La Escala (Gerona)', false, 'GER'],
+      [36.902915, -4.113961, 'img/marker_green.png', 'Castillo de Zalia', 'Alcaucin, Málaga', false, '1'],
+      [36.014321, -5.60445, 'img/marker_green.png', 'Basílica de Baelo Claudia', 'Tarifa, Cádiz', false, '2'],
+      [37.435211, -6.042773, 'img/marker_green.png', 'Benaki Museum', 'Koumpari 1, Athens, Greece', false, '2']
     ]
 /*    origins: [
       ['37.936294', '23.947394'],
       ['37.975669', '23.733868']
     ] /*para rutas*/
   });
-  // End of map
 
-  // Begin of form validation
 
-  // End of form validation
+// Begin of form validation
+var formulario = document.getElementById("myForm");
+window.onload = iniciar;
+
+function iniciar(){
+    document.getElementById("submit1").addEventListener('click', validar , false)
+}
+//VALIDACION NOMBRE Y APELLIDOS
+function validaNombre(){
+    var elemento = document.getElementById("username");
+    if (elemento.value ==""){
+        alert("Nombre y apellidos Obligatorio");
+        return false;
+    }
+    return true;
+}
+
+
+// VALIDACION EMAIL
+function validaEmail(){
+    var elemento = document.getElementById("usermail");
+    if (elemento.value ==""){
+        alert("Email Obligatorio");
+        return false;
+    }
+    return true;
+}
+
+
+//CONTRASEÑA y REPETIR CONTRASEÑA
+
+
+
+
+function comprobarClave(){ 
+   /* var p1 = document.myForm.userpass.value 
+    var p2 = document.myForm.userpass2.value */
+
+    var p1 = document.getElementById("userpass").value;
+    var p2 = document.getElementById("userpass2").value;
+
+    if (p1 == p2) 
+        alert("Las dos claves son iguales...\nRealizaríamos las acciones del caso positivo") 
+    else 
+        alert("Las dos claves son distintas...\nRealizaríamos las acciones del caso negativo") 
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+/*function validaContrasena(){
+    var elemento = document.getElementById("userpass");
+    if (isNaN(elemento.value =="" )){
+        alert("Contraseña Obligatoria solo con valor numéricos");
+        return false;
+    }
+    return true;
+}
+
+//REPITA CONTRASEÑA
+function validaContrasena2(){
+    var elemento = document.getElementById("userpass2");
+    if (elemento.value ==""){
+        alert("");
+        return false;
+    }
+    return true;
+}*/
+
+//FECHA NACIMIENTO
+function validaBday(){
+    var elemento = document.getElementById("userbdate");
+
+    if (elemento.value ==""){
+        alert("Introduzca una fecha");
+        return false;
+    }
+    return true;
+}
+
+
+
+function validar (e){
+    if ( validaNombre() && validaEmail() && validaContrasena() && validaBday() && confirm("Pulsa aceptar si deseas enviar el formulario")){
+            return true;
+        }else{
+            e.preventDefault();
+        return false;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var validate = function() {
+   var form = document.getElementById("theForm");
+   if (!form.aInteger.value || isNaN(parseInt(form.aInteger.value))) {
+      return false;
+   }
+   if (!form.aFloat.value || isNaN(parseFloat(form.aFloat.value))) {
+      return false;
+   }
+   if (form.tenChars.value.length < 10) {
+      return false;
+   }
+   var emailRegex = /^\w+@\w+\.\w+$/;
+   if (!form.email.value.match(emailRegex)) {
+      return false;
+   }
+   if (form.aSelect.value=="Select one"){
+      return false;
+   }
+   return true;
+}
+*/
+// End of form validation
 
 /*Animations*/
 /*Menú hamburguesa*/
@@ -86,3 +240,5 @@ $(document).ready(function() {
   });
 
 });
+
+
