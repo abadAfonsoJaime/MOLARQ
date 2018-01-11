938400ffb80d52a9811ee29fb5b15f7aee8adb13
Unknown <cristina.aznarquesada@gmail.com>
2018-01-11 15:49:29 +0100

    Modal window

--- js/plugin.js
+++ js/plugin.js
@@ -49,6 +49,32 @@ document.getElementById("Cronologia").value = "default";
       }
   });
 
+
+  /*Ventana Modal*/
+var modal = document.getElementById('myModal');
+          // boton que abre el modal
+var btn = document.getElementById("myBtn");
+          // span que cierra el modal
+var span = document.getElementsByClassName("close")[0];
+
+          // cuando clickeo el boton abro el modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+          // cuando clickeo el span cierro el modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+          // cuando clickeo fuera del modal, lo cierro
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
 // Barra fija al hacer scroll
     $(window).scroll(function(){
     if ($(window).scrollTop() >= 72) { // El valor debe ser igual a la altura del header
@@ -84,4 +110,4 @@ document.getElementById("Cronologia").value = "default";
     } // End if
   });
 
-});
+}); /*End of document.ready*/
