e34b6cf2c9b522a785827d32a901ae0216f4c433
Unknown <jaime.abad.afonso@gmail.com>
2018-01-29 15:06:48 +0100

    AJAX get Yacimientos

--- js/saveJSONdata.js
+++ js/saveJSONdata.js
@@ -1,20 +1,18 @@
-var listaDatos = [];
+document.querySelector('#saveDataBtn').addEventListener('click',saveData);
 
-function upDate(postTitulo, postDes1, postDes2, postLat, postLong, postRegion, postCronologia){
+function saveData(){
+	var saveTitle = document.querySelector('#title').value,
+	 saveDes1 = document.querySelector('#comment').value,
+	 saveDes2 = document.querySelector('#comment2').value,
+	 saveLat = document.querySelector('#latitud').value,
+	 saveLong = document.querySelector('#longitud').value,
+	 saveRegion = document.querySelector('#region').value,
+	 saveCronologia = document.querySelector('#period').value,
+	 saveUserID = "";
 
-	var obtener = {
-		title : postTitulo, 
-		des1 : postDes1,
-		des2 : postDes2,
-		lat : postLat,
-		long : postLong,
-		region : postRegion,
-		cronologia : postCronologia,
 
-	};
-	
 
-	console.log(obtener);
-	listaDatos.push(obtener);
-	console.log(listaDatos);
+	upDate( saveTitle, saveDes1, saveDes2, saveLat, saveLong, saveRegion, saveCronologia, saveUserID );
 }
+
+
